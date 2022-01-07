fd3cb1f935cf540f8d847dcf9997dd4af854a867
Selma Heric <selma@balkon.ba>
2022-01-07 17:51:58 +0100

    add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  PORT: process.env.PORT || 3001,
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_NAME: process.env.DB_NAME || 'imdb',
+  DB_USERNAME: process.env.DB_USERNAME || 'postgres',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'password',
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(1)
+const express = require('express');
+
+const config = require('./config');
+
+const app = express();
+const port = config.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+})
