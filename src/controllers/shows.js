a2e27d8d99f018410d3e134d5003e1d51643c528
Selma Heric <selma@balkon.ba>
2022-01-09 16:26:47 +0100

    handle db error

--- src/controllers/shows.js
+++ src/controllers/shows.js
@@ -8,106 +8,110 @@ const router = express.Router();
 router.get(
   '/',
   validateQuery(getShowsSchema),
-  async (req, res) => {
-    const {
-      type, limit, offset, search, searchByPhrase,
-    } = req.query;
+  async (req, res, next) => {
+    try {
+      const {
+        type, limit, offset, search, searchByPhrase,
+      } = req.query;
 
-    const dbQueryParams = {
-      type,
-      limit,
-      offset,
-    };
+      const dbQueryParams = {
+        type,
+        limit,
+        offset,
+      };
 
-    let query = 'SELECT *, release_date::text FROM shows WHERE type = :type';
+      let query = 'SELECT *, release_date::text FROM shows WHERE type = :type';
 
-    if (req.user) {
-      query = `
+      if (req.user) {
+        query = `
       SELECT
         *,
         release_date::text,
         (SELECT rating FROM ratings where id_user = :idUser and shows.id = ratings.id_show) as my_rating
       FROM shows WHERE type = :type`;
-      dbQueryParams.idUser = req.user.id;
-    }
-
-    let queryCount = 'SELECT count(*) FROM shows where type = :type';
-
-    if (search && !searchByPhrase) {
-      const searchArray = search.split(' ');
-
-      const searchFormatted = [];
-      for (let i = 0; i < searchArray.length; i += 1) {
-        searchFormatted.push(`%${searchArray[i]}%`);
+        dbQueryParams.idUser = req.user.id;
       }
 
-      query += ' AND (title ILIKE ANY (:search) OR description ILIKE ANY (:search))';
-      queryCount += ' AND (title ILIKE ANY (:search) OR description ILIKE ANY (:search))';
-      dbQueryParams.search = searchFormatted;
-    }
+      let queryCount = 'SELECT count(*) FROM shows where type = :type';
 
-    let searchByPhraseValid = true;
+      if (search && !searchByPhrase) {
+        const searchArray = search.split(' ');
 
-    if (search && searchByPhrase) {
-      const isBefore = search.includes('before');
-      const isAfter = search.includes('after');
-      const isStars = search.includes('star');
-      if (isBefore || isAfter) {
-        /**
-        * Handle by date
-         */
-        const dateMatch = search.match(/ \d+/g);
-        const date = dateMatch ? new Date(dateMatch[0]) : null;
-        if (!date || Number.isNaN(date.getTime())) {
-          searchByPhraseValid = false;
-        } else {
-          const operator = isBefore ? '<=' : '>=';
-          query += ` AND date_trunc('year', release_date::date) ${operator} date_trunc('year', :dateParam::date)`;
-          queryCount += ` AND date_trunc('year', release_date::date) ${operator} date_trunc('year', :dateParam::date)`;
-          dbQueryParams.dateParam = date;
+        const searchFormatted = [];
+        for (let i = 0; i < searchArray.length; i += 1) {
+          searchFormatted.push(`%${searchArray[i]}%`);
         }
-      } else if (isStars) {
-        /**
-         * Handle by starts
-         */
-        const atLeast = search.includes('at least') || search.includes('more than');
-        const atMost = search.includes('at most') || search.includes('less than');
-        const starsNumber = search.match(/^\d | \d+/g);
-        if (!starsNumber) {
-          searchByPhraseValid = false;
-        } else {
-          let operator = '=';
-          if (atLeast) {
-            operator = '>=';
-          }
-          if (atMost) {
-            operator = '<=';
-          }
-          query += ` AND average_rating ${operator} :starsParam`;
-          queryCount += ` AND average_rating ${operator} :starsParam`;
-          dbQueryParams.starsParam = Number(starsNumber[0]);
-        }
-      } else {
-        searchByPhraseValid = false;
+
+        query += ' AND (title ILIKE ANY (:search) OR description ILIKE ANY (:search))';
+        queryCount += ' AND (title ILIKE ANY (:search) OR description ILIKE ANY (:search))';
+        dbQueryParams.search = searchFormatted;
       }
+
+      let searchByPhraseValid = true;
+
+      if (search && searchByPhrase) {
+        const isBefore = search.includes('before');
+        const isAfter = search.includes('after');
+        const isStars = search.includes('star');
+        if (isBefore || isAfter) {
+          /**
+          * Handle by date
+           */
+          const dateMatch = search.match(/ \d+/g);
+          const date = dateMatch ? new Date(dateMatch[0]) : null;
+          if (!date || Number.isNaN(date.getTime())) {
+            searchByPhraseValid = false;
+          } else {
+            const operator = isBefore ? '<=' : '>=';
+            query += ` AND date_trunc('year', release_date::date) ${operator} date_trunc('year', :dateParam::date)`;
+            queryCount += ` AND date_trunc('year', release_date::date) ${operator} date_trunc('year', :dateParam::date)`;
+            dbQueryParams.dateParam = date;
+          }
+        } else if (isStars) {
+          /**
+           * Handle by starts
+           */
+          const atLeast = search.includes('at least') || search.includes('more than');
+          const atMost = search.includes('at most') || search.includes('less than');
+          const starsNumber = search.match(/^\d | \d+/g);
+          if (!starsNumber) {
+            searchByPhraseValid = false;
+          } else {
+            let operator = '=';
+            if (atLeast) {
+              operator = '>=';
+            }
+            if (atMost) {
+              operator = '<=';
+            }
+            query += ` AND average_rating ${operator} :starsParam`;
+            queryCount += ` AND average_rating ${operator} :starsParam`;
+            dbQueryParams.starsParam = Number(starsNumber[0]);
+          }
+        } else {
+          searchByPhraseValid = false;
+        }
+      }
+
+      query += ' ORDER BY average_rating DESC LIMIT :limit OFFSET :offset';
+
+      const response = await db.connection.raw(query, dbQueryParams);
+      const responseCount = await db.connection.raw(queryCount, dbQueryParams);
+
+      const shows = searchByPhraseValid ? response.rows : [];
+
+      res.send({
+        message: 'Successfully fetched shows!',
+        data: {
+          shows,
+          limit,
+          offset,
+          count: responseCount.rows && responseCount.rows[0] ? +responseCount.rows[0].count : 0,
+        },
+      });
+    } catch (error) {
+      next(error);
     }
-
-    query += ' ORDER BY average_rating DESC LIMIT :limit OFFSET :offset';
-
-    const response = await db.connection.raw(query, dbQueryParams);
-    const responseCount = await db.connection.raw(queryCount, dbQueryParams);
-
-    const shows = searchByPhraseValid ? response.rows : [];
-
-    res.send({
-      message: 'Successfully fetched shows!',
-      data: {
-        shows,
-        limit,
-        offset,
-        count: responseCount.rows && responseCount.rows[0] ? +responseCount.rows[0].count : 0,
-      },
-    });
   },
 );
 
